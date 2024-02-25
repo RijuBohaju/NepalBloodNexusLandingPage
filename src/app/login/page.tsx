377f391718498859931a4b7220165c65f26c2e22
RijuBohaju <rezu.rezu.12@gmail.com>
2024-02-25 12:08:15 +0545

    First commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,12 +8,12 @@ export default async function LoginPage() {
   return (
     <>
       <Header />
-      <section className="bg-ct-blue-600 min-h-screen grid place-items-center">
+      <section className="bg-red-20 min-h-screen grid place-items-center">
         <div className="w-full">
-          <h1 className="text-4xl lg:text-6xl text-center font-[600] text-ct-yellow-600 mb-4">
+          <h1 className="text-4xl lg:text-6xl text-center font-[600] text-red-600 mb-4">
             Welcome Back
           </h1>
-          <h2 className="text-lg text-center mb-4 text-ct-dark-200">
+          <h2 className="text-lg text-center mb-4 text-ct-dark-210">
             Login to have access
           </h2>
           <LoginForm />
